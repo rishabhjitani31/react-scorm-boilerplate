b7b4be1daaacc4f270dc4fc679cbf8d064f3de14
rishabh_jitani <rishabh.j@simformsolutions.com>
2019-08-06 18:02:32 +0530

    first commit

---
+++ app/config/samples.js
@@ -0,0 +1,75 @@
+export const quiz_example = {
+  "title":"Quiz Example",
+  "questions":[
+    {
+      "type":"multiple_choice",
+      "value":"What is 12*12?",
+      "choices":[
+        {
+          "id":"1",
+          "value":"144",
+          "answer":true,
+        }, {
+          "id":"2",
+          "value":"100",
+          "answer":false,
+        }, {
+          "id":"3",
+          "value":"88",
+          "answer":true,
+        }, {
+          "id":"4",
+          "value":"96",
+          "answer":false,
+        },
+      ],
+      "difficulty":2,
+    }, {
+      "type":"multiple_choice",
+      "value":"what is india's national animal?",
+      "choices":[
+        {
+          "id":"1",
+          "value":"Cheetah",
+          "answer":false,
+        }, {
+          "id":"2",
+          "value":"Lion",
+          "answer":false,
+        }, {
+          "id":"3",
+          "value":"Bengal Tiger",
+          "answer":true,
+        }, {
+          "id":"4",
+          "value":"Tiger",
+          "answer":true,
+        },
+      ],
+      "difficulty":5,
+    }, {
+      "type":"multiple_choice",
+      "value":"Which US President's office commissioned the creation of SCORM?",
+      "choices":[
+        {
+          "id":"1",
+          "value":"Donald Trump",
+          "answer":false,
+        }, {
+          "id":"2",
+          "value":"Barack Obama",
+          "answer":false,
+        }, {
+          "id":"3",
+          "value":"Ronald Reagan",
+          "answer":false,
+        }, {
+          "id":"4",
+          "value":"Bill Clinton",
+          "answer":true,
+        },
+      ],
+      "difficulty":8,
+    },
+  ],
+};
